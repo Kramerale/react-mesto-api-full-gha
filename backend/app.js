1ff3f79e1819ba5cf38fbe7f782806dd704c2592
Kramerale <aliceshoolz96@gmail.com>
2023-11-24 17:26:32 +0400

    test8

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
